209950e06d390379c77cb80224f926d5c0dd6235
Juan319-u <80417972+Juan319-u@users.noreply.github.com>
2021-09-22 17:14:17 -0500

    Add files via upload

---
+++ Punto2.R
@@ -0,0 +1,8 @@
+'Este codigo Calcula e imprime la sumatoria del punto e'
+'Parametros de entredad:'
+'Autor: Juan Felipe Corrales Toro'
+'ULTIMA ACTUALIZACION: 22 de Septiembre / 2021'
+x=10 #aca podemos cambiar los valores
+n=10 #aca ingresamos los terminos de n a calcular
+w=(((factorial(1:n)**(1:n))*x**(1:n))/(factorial(2*1:n)))
+sum(w)#realizamos la sumatoria
